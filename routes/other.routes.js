b4b1e367af4e5510641317f4d078bbf4061117ee
veragusttavo3 <veragusttavo3@gmail.com>
2024-02-19 23:57:09 -0300

    agrego archivos y no pude completar con mvc

---
+++ routes/other.routes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+const otherController = require("../controllers/other");
+
+//  "/"
+router.get("/", otherController.home);
+router.get("/home", (req, res) => res.redirect("/"));
+router.get("/menu", otherController.menu);
+
+module.exports = router;
